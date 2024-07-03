eda194ff4d544d2e55f323ea7789b8c26fb3cbd5
Nohinkyu <wjd2256@naver.com>
2024-07-03 20:32:26 +0900

    feat: sharedPreference 구현

---
+++ app/src/main/java/com/devik/homebarordermanager/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.devik.homebarordermanager.util
+
+object Constants {
+
+    const val PREFERENCE_NAME = "com.devik.homebarordermanager.PREFERENCE_KEY"
+
+    const val KEY_MAIL_ADDRESS = "mail_address"
+    const val KEY_PROFILE_IMAGE = "profile_image"
+}
